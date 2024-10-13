10fdeb5975442abae83fe45c9231ef18d9d573d0
Krishnakant1101 <krishnakant@thoughtwin.com>
2024-10-14 00:59:02 +0530

    Update Example_02.js

--- Topic-Closure/Example_02.js
+++ Topic-Closure/Example_02.js
@@ -10,3 +10,19 @@ const x=outer();
 const y=x();
 
 console.log("value : ",y);
+
+
+/*......................................................................................................................................*/
+
+const outer=()=>{
+    value=22;
+    const inner=()=>{
+        return value;
+    }
+    return inner;
+}
+
+const x=outer();
+const y=x();
+
+console.log("value : ",y);
