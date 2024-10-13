5c6e944e2a04b11d6c188286015702a205757fc4
Krishnakant1101 <krishnakant@thoughtwin.com>
2024-10-14 01:00:43 +0530

    Update Example_03.js

--- Topic-Closure/Example_03.js
+++ Topic-Closure/Example_03.js
@@ -9,3 +9,18 @@ function outer(x){
 
 const child=outer(12);
 child(78);
+
+
+/*.........................................................................................................................................*/
+
+const outer=(x)=>{
+
+    const inner=(y)=>{
+        console.log("X : ",x," and Y : ",y);
+    }
+    return inner;
+
+}
+
+const child=outer(12);
+child(78);
