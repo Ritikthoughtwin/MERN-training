53472adb3b7e24b957dd6f37598282c49a43a316
Krishnakant1101 <krishnakant@thoughtwin.com>
2024-10-16 14:29:44 +0530

    improve description and code

--- Topic-Hoisting/function_hoisting.js
+++ Topic-Hoisting/function_hoisting.js
@@ -1,21 +1,32 @@
+/*
+Function hoisting in JavaScript is a process that moves function 
+declarations to the top of their scope before the code is executed
+*/
+/*.................................................................................................................................................................................*/
 
-printHello();
-function printHello(){   //will work with regular function
+
+printHello();  // This will work because function declarations are hoisted
+
+function printHello() {
     console.log("Hello");
 }
 
-/*..................................................................................................................................................*/
+// Function Expression with let and const (Not Hoisted)
+ 
+ printHii(); // will throw error
 
-printHii();
-//use let,var or const
-let printHii=function (){   //will not work
+let printHii = function () {
     console.log("Hii");
-}
+};
 
-/*.............................................................................................................................................*/
+printHii();  // This will work because it's called after the assignment
 
-printGoodMorning();
-//use let,var or const
-let printGoodMorning=()=>{   //will not work
+// Arrow Function Expression with let and const (Not Hoisted)
+
+ printGoodMorning();// will throw error
+
+let printGoodMorning = () => {
     console.log("GoodMorning");
-}
+};
+
+printGoodMorning();  // This will work because it's called after the assignment
