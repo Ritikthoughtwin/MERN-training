566d9cc74df72ba6c43cf7f6c0e1812e52605085
Krishnakant1101 <krishnakant@thoughtwin.com>
2024-10-16 13:45:20 +0530

    add description and improve naming convention

--- Topic-Closure/Example_01.js
+++ Topic-Closure/Example_01.js
@@ -1,23 +1,36 @@
-function parent() {
-    const name = "krishnakant Thakur";
-    function child() {
-        console.log("Name : ", name);
+
+/*
+JavaScript Closure Explained. A closure in JavaScript is a function that 
+has access to variables in its parent scope, even after the parent function 
+has returned. Closures are created when a function is defined inside another 
+function, and the inner function retains access to the variables in the outer 
+function's scope.
+*/
+
+/*.................................................................................................................................................................................................*/
+
+
+function displayPersonInfo() {
+    const personName = "Krishnakant Thakur"; 
+
+    function showName() {
+        console.log("Person's Name: ", personName); 
     }
-    child();
+
+    showName(); 
 }
 
-parent();
+displayPersonInfo(); 
 
-/*...................................................................................................................*/
+/*........................................................................................................................................................................................................................................*/
 
-const outer=()=>{
-    value=5;
-     const inner=()=>{
-         console.log(value);      //using arrow function
-     }
-    inner();
-}
-outer();
+const outerFunction = () => {
+    const numberValue = 5; 
+    const innerFunction = () => {
+        console.log("Value: ", numberValue); 
+    };
 
-    
+    innerFunction(); 
+};
 
+outerFunction(); 
