7b2e07b7fdd76a1296599af12f11f3942c5ab2b8
Krishnakant1101 <krishnakant@thoughtwin.com>
2024-10-16 14:44:33 +0530

    add defination

--- Topic-Hoisting/variable_hoisting.js
+++ Topic-Hoisting/variable_hoisting.js
@@ -1,3 +1,9 @@
+/*
+Variable hoisting in JavaScript is the process of moving a variable's declaration to the top of 
+its scope before the code is executed. This allows the variable to be used before it has been declared
+in the code. However, the variable's value will be undefined until it is assigned a value.
+*/
+
 //variable hoisting with var
 
 console.log(x); // Output: undefined (due to hoisting)
